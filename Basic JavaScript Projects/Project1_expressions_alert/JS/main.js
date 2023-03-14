2f4e2cc44b71cacf0f5f212cb37681c557a41a83
Danielhmiller <danielmillerusmc3043@outlook.com>
2023-03-14 15:52:18 -0500

    First Submission Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+document.write("Hello, World!"); //basic text
+
+var A = "This is a string";
+
+document.write(A); //variable called from a string
+
+var B = "This is also a string"
+
+window.alert(B); //varible called again but this time in the form of a window alert
+
+document.write("Daniel told Jada, \"Stop, Jada! You're being mean!\" </br> \"IDC Jada you're goofy anyway!\" Daniel responded."); //text with forward slashes added to include the "" to show speaking 
+
+document.write("\"Be one with yourself,"
++ "Knowing yourself leads to self-improvement,"
++ "Self-improvement helps you become a better YOU\""); //same as above with additional text
+
+var Friends = "The Gang", AFriend = "Ryan", BFriend = "David'";
+document.write(AFriend); // creating a variable and then calling that same variable
+
+4+4;
+document.write(4+4); //addition
